fd0fb3e999c037d82ed08bde766c364d4894be37
Ingrid Maciel <ingridmaciel5715@gmail.com>
2025-01-08 11:39:48 -0300

    :construction: Criando as perguntas do quiz

--- Quiz em Python/quiz.py
+++ Quiz em Python/quiz.py
@@ -7,7 +7,6 @@ print("Olá! Seja bem-vindo ao Quiz de Avatar: A Lenda de Aang!")
 userName = input("Primeiro, digite seu nome: \n")
 print(f"Seu nome é {userName}")
 
-
 #Pergunta ao usuário e recebe resposta
 print("Pronto para começar?")
 answerUser = input("Digite sim ou não (S / N): \n") 
@@ -19,7 +18,8 @@ if answerUser == "N" or answerUser == "n":
 #Variável para contar os pontos
 score = 0
 
-#Criar pergunta com alternativas
+print("Responda a cada pergunta de acordo com as alternativas")
+#Primeira pergunta
 print("1. Em Avatar, quais os elementos de dobra existentes? \n")
 print("(A) Água, Terra, Fogo e Ar")
 print("(B) Terra, Fogo, Metal e Água")
@@ -36,22 +36,41 @@ if answer1 == "A" or answer1 == "a":
 else:
     print("Resposta errada! \n Quem sabe da próxima vez?")
 
-# Adicionar mais perguntas
+#Segunda pergunta
 print("2. O que é um Avatar? \n")
 print("(A) Um mestre dos 4 elementos")
 print("(B) Uma ponte entre o mundo dos humanos e o mundo dos espíritos")
-print("(C) Um ")
+print("(C) Um pacificador")
 print("(D) Todas as opções anteriores")
 
-#Receber resposta do usuário
-answer1 = input("Resposta: ")
+#Recebe resposta do usuário
+answer2 = input("Resposta: ")
 
 #Conferir a resposta
-if answer1 == "A" or answer1 == "a":
+if answer2 == "D" or answer2 == "d":
     print("Você acertou! \n Parabéns!!!")
     score += 10
 else:
     print("Resposta errada! \n Quem sabe da próxima vez?")
+
+#Terceira pergunta
+print("3. Quais os nomes dos irmãos que encontraram Aang preso no iceberg? \n")
+print("(A) Kiana e Sora")
+print("(B) Katara e Sokka")
+print("(C) Zuko e Azula")
+print("(D) Sokka e Toph")
+
+#Recebe resposta do usuário
+answer3 = input("Resposta: ")
+
+#Conferir a resposta
+if answer3 == "B" or answer2 == "b":
+    print("Você acertou! \n Parabéns!!!")
+    score += 10
+else:
+    print("Resposta errada! \n Quem sabe da próxima vez?")
+
+
 
 print("Término do Quiz!")
 print(f"Pontuação final: \n O jogador {userName} fez {score} pontos")
